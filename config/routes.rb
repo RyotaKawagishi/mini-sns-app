14bd522eae77cc222aeeac938e44e1501afa2777
Ryota Kawagishi <ryota.kawagishi@techouse.jp>
2025-04-10 17:21:22 +0900

    13.3

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  get 'password_resets/new'
-  get 'password_resets/edit'
-
+ 
   root "static_pages#home"
 
   get  "/help",    to: "static_pages#help"
@@ -14,9 +12,16 @@ Rails.application.routes.draw do
   post "/login", to: "sessions#create"
   delete "/logout", to: "sessions#destroy"
 
+  get 'password_resets/new'
+  get 'password_resets/edit'
+
+  get '/microposts', to: 'static_pages#home'
+
   resources :users
   resources :account_activations, only: [:edit]
   resources :password_resets, only:[:new, :create, :edit, :update]
+  resources :microposts, only:[:create, :destroy]
 
+  
 
 end
